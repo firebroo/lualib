d374c26580bedbedfc743d36f46f51ca1e2b0814
firebroo <admin@firebroo.com>
2019-03-29 17:30:38 +0800

    update

--- src/utility.lua
+++ src/utility.lua
@@ -14,7 +14,7 @@ function _M.clone(object)
 
         local new_table = {}
         lookup_table[object] = new_table
-	    
+        
         for key, value in pairs(object) do
             new_table[_copy(key)] = _copy(value)
         end
@@ -24,7 +24,7 @@ function _M.clone(object)
     return setmetatable(_copy(object), mt)
 end
 
-function _M.split(str, pat)
+function _M.str_split(str, pat)
     local t = {}
     local fpat = "(.-)" .. pat
     local last_end = 1
@@ -63,6 +63,64 @@ function _M.str_explode_one(str, pat)
         return str, nil
     end
     return str:sub(1, index-1), str:sub(index+1, -1)
+end
+
+function _M.str_trim(s) 
+    return string.gsub(s, "^%s*(.-)%s*$", "%1")
+end
+
+function _M.var_dump(var, level)
+    local split_char     = '\r\n'
+    local split_table    = '    '
+    local sz_ret         = ''
+    
+    if level == nil then
+        level = 0
+    end
+    
+    local function tabs(n)
+        local sz_tabs = ''
+        for i = 0, n do
+            sz_tabs = sz_tabs .. split_table
+        end
+
+        return sz_tabs
+    end
+    
+    local function dump(var, ret)
+        for k, v in pairs(var) do
+            local sz_key = _M.var_dump(k)
+            local sz_val = _M.var_dump(v)
+            if (sz_key and sz_val) then
+                ret = string.format("%s%s[%s] = %s,%s", ret, tabs(level), 
+                     _M.var_dump(k), _M.var_dump(v, level+1), split_char)
+            end
+        end     
+        return ret
+    end
+
+    local sz_type = type(var)
+    if sz_type == "number" then
+        sz_ret = sz_ret .. var
+    elseif sz_type == "boolean" then
+        sz_ret = sz_ret .. tostring(var)
+    elseif sz_type == "string" then
+        sz_ret = sz_ret .. string.format("%q", var)
+    elseif sz_type == "table" then
+        sz_ret = sz_ret .. "{" .. split_char
+        sz_ret = dump(var, sz_ret)
+        local metatable = getmetatable(var)
+        if metatable ~= nil and type(metatable.__index) == "table" then
+            sz_ret = dump(metatable.__index, sz_ret)
+        end
+        sz_ret = sz_ret .. tabs(level-1) .. "}"
+    elseif sz_type == "function" then
+        sz_ret = sz_ret .. tostring(var)
+    elseif sz_type == "nil" then
+        return nil
+    end
+
+   return sz_ret
 end
 
 return _M
